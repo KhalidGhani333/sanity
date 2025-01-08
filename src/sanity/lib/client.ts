9c504d2ee7a0cfda6efdbeb245939ad3e7c4b382
Khalid Ghani <khalidghani.333@gamil.com>
2025-01-08 12:39:51 +0500

    push code

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,8 +3,8 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId: "ljyjqkvs", 
+  dataset: 'production',             
+  apiVersion: 'v2025-01-05', 
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
